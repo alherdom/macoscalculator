ecb053ba36f31965190d4e7e48390dbf4cd3028e
alherdom <alejandroh1@live.com>
2023-06-25 01:02:48 +0100

    Sync

--- js/index.js
+++ js/index.js
@@ -3,9 +3,6 @@ const buttons = document.querySelectorAll('.btn');
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        if (button.id === 'point' && display.textContent.includes('.')) {
-            return;
-        }
         if (button.id === 'c') {
             display.textContent = '0';
         }
@@ -29,32 +26,32 @@ buttons.forEach(button => {
 
 function evaluateExpression(expression) {
     const operators = {
-        '+': (a, b) => a + b,
-        '-': (a, b) => a - b,
-        'x': (a, b) => a * b,
-        '÷': (a, b) => a / b,
-        '%': (a, b) => a / 100
+      '+': (a, b) => a + b,
+      '-': (a, b) => a - b,
+      'x': (a, b) => a * b,
+      '÷': (a, b) => a / b,
+      '%': (a, b) => a / 100
     };
-
-    const parts = expression.split(/(\+|\-|x|÷|%)/);
+  
+    const parts = expression.match(/(\d+(\.\d+)?|\+|\-|x|÷|%)/g);
     let result = parseFloat(parts[0]);
     let operator = null;
-
+  
     for (let i = 1; i < parts.length; i += 2) {
-        const value = parseFloat(parts[i + 1]);
-        const operation = operators[parts[i]];
-
-        if (operator === null) {
-            result = operation(result, value);
-        } else {
-            result = operators[operator](result, value);
-            operator = null;
-        }
-
-        if (i + 2 < parts.length) {
-            operator = parts[i + 2];
-        }
+      const value = parseFloat(parts[i + 1]);
+      const operation = operators[parts[i]];
+  
+      if (operator === null) {
+        result = operation(result, value);
+      } else {
+        result = operators[operator](result, value);
+        operator = null;
+      }
+  
+      if (i + 2 < parts.length) {
+        operator = parts[i + 2];
+      }
     }
-
-    return result;
-}
+  
+    return result.toString();
+  }
